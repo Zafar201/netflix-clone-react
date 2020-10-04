62af2a48131cedb03248e69dff28c127399e7717
Zafar Irshad <zafarirshad201@gmail.com>
2020-10-04 12:59:30 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchNetflixOriginals}/>
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
       <Row title="Romace Movies" fetchUrl={requests.fetchTopRated}/>
       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
